b7f800fc759a01ce915dac5b8425bc5ea66d7d79
Lilian (Guo Li) <guoli_dlmu@163.com>
2023-11-26 14:11:25 -0400

    Update myIntro.jsx
    (This is dev environment)

--- src/pages/myIntro.jsx
+++ src/pages/myIntro.jsx
@@ -3,7 +3,7 @@ import me from '../pics/myphoto_resize2.jpg'
 
 
 function MyIntro (){
-    const text = 'My name is Lilian, I come from China. I graduated from Dalian Maritime University and my major is the Internet of Things. It is my first time to Canada, I\'m exticed and happy to come here and join this course. I am outgoing person who really enjoys cooking, cleaning and drawing. I also like outdoor activities, like hiking and camping.\n'
+    const text = '(This is dev environment) My name is Lilian, I come from China. I graduated from Dalian Maritime University and my major is the Internet of Things. It is my first time to Canada, I\'m exticed and happy to come here and join this course. I am outgoing person who really enjoys cooking, cleaning and drawing. I also like outdoor activities, like hiking and camping.\n'
     const Navigate = useNavigate() 
     const Hometown = () =>{
         Navigate('/hometown')
